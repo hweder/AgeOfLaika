0eb158a118bae31efc32fec8b5f6d7f47b6d0fab
hweder <hermann@Hermanns-Macbook-Air.local>
2014-09-29 21:51:29 +0200

    first revision of AgeOfLaika

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var convertedDogYearsLabel: UILabel!
+    
+    
+   
+    @IBOutlet weak var enterHumanYearsTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
+        let humanYearsFromTextField = enterHumanYearsTextField.text.toInt()!
+        enterHumanYearsTextField.resignFirstResponder()
+        let conversionConstant = 7
+        convertedDogYearsLabel.hidden = false
+        let convertedDogYearsString = "\(humanYearsFromTextField * conversionConstant)" + " dog years for " + "\(humanYearsFromTextField)" + " human years"
+        convertedDogYearsLabel.text =  convertedDogYearsString
+        
+        
+    }
+   
+    @IBAction func convertToRealDogYearsButtonPressed(sender: UIButton) {
+        let humanYearsFromTextField = enterHumanYearsTextField.text.toInt()!
+        enterHumanYearsTextField.resignFirstResponder()
+        let conversionConstant = 7
+        convertedDogYearsLabel.hidden = false
+        let convertedDogYearsString = "\(humanYearsFromTextField * conversionConstant)" + " dog years for " + "\(humanYearsFromTextField)" + " human years"
+        convertedDogYearsLabel.text =  convertedDogYearsString
+        
+        
+    }
 }
 
